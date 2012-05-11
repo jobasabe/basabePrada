ed29975bba2808f86c40f08e364e474c995e1bad
José Luis Basabe Villalobos <jose@jlbv.(none)>
2012-05-11 17:29:14 -0430

    commit inicial

---
+++ basabePrada/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasabePrada::Application.config.secret_token = 'c8066889bb43a60aeea3d2887b78b1236641d94add81ccb2463945fbf524573b4b3965db0373c3b9b7a295742c96a1ed7b02887adeec747a907aa4c804ecf259'
